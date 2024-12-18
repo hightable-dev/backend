94d61b3d8cb8d8f5a09922181c037420de51ff5e
admin <admin@hightable.ai>
2024-12-18 10:27:04 +0530

    hightable changes add

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -34,7 +34,7 @@ module.exports.bootstrap = async function() {
 
   // This bootstrap version indicates what version of fake data we're dealing with here.
   var HARD_CODED_DATA_VERSION = 0;
-
+// 
   // This path indicates where to store/look for the JSON file that tracks the "last run bootstrap info"
   // locally on this development computer (if we happen to be on a development computer).
   var bootstrapLastRunInfoPath = path.resolve(sails.config.appPath, '.tmp/bootstrap-version.json');
